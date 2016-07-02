a98a42c99e6aca43f45c0ff4d63b086cf556b1a8
zzh1996 <zzh1996@mail.ustc.edu.cn>
2016-07-02 14:30:16 +0800

    add create_views script

---
+++ scripts/create_views.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import MySQLdb
+
+db = MySQLdb.connect(host=input('host:'),
+                     user=input('user:'),
+                     passwd=input('password:'),
+                     db=input('db:'))
+
+db.autocommit(True)
+
+cur = db.cursor()
+
+cur.execute("""
+    CREATE OR REPLACE VIEW radius.lastmonthtraffic AS
+        select
+            radius.radacct.username AS UserName,
+            (sum((radius.radacct.acctinputoctets + radius.radacct.acctoutputoctets))) AS TrafficSum
+        from
+            radius.radacct
+        where
+            ((month(radius.radacct.acctstarttime) = month(date_sub(now(),interval 1 month))) and
+            (year(radius.radacct.acctstarttime) = year(date_sub(now(),interval 1 month))))
+        group by
+            radius.radacct.username
+        order by
+            (sum((radius.radacct.acctinputoctets + radius.radacct.acctoutputoctets)));
+    """)
+cur.execute("""
+        CREATE OR REPLACE VIEW radius.monthtraffic AS
+        select
+            radius.radacct.username AS UserName,
+            (sum((radius.radacct.acctinputoctets + radius.radacct.acctoutputoctets))) AS TrafficSum
+        from
+            radius.radacct
+        where
+            ((month(radius.radacct.acctstarttime) = month(now())) and
+            (year(radius.radacct.acctstarttime) = year(now())))
+        group by
+            radius.radacct.username
+        order by
+            (sum((radius.radacct.acctinputoctets + radius.radacct.acctoutputoctets)));
+        """)
+
+db.close()
