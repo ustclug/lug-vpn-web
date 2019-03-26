402619cc5ff3540df26d22f6255e5589107c0a3c
zzh1996 <zhengzihan1996@gmail.com>
2019-03-27 01:53:55 +0800

    add hint on student no.

--- app/forms.py
+++ app/forms.py
@@ -18,7 +18,7 @@ class LoginForm(FlaskForm):
 
 class ApplyForm(FlaskForm):
     name = StringField('Name in native language (eg. 张三)', [InputRequired()])
-    studentno = StringField('Student/Staff No.', [InputRequired()])
+    studentno = StringField('Student/Staff No. (eg. PB18000001)', [InputRequired()])
     phone = StringField('Phone', [InputRequired()])
     reason = TextAreaField('Apply reason (please specify the criteria which you meet)', [InputRequired()])
     agree = BooleanField('I agree to the following constitution')
