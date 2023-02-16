24ef1ebd6d0658587c9d3b2b06bb2d59512ec8fe
taoky <me@taoky.moe>
2023-02-16 22:56:41 +0800

    Update dependencies

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from app import app
 from app import db
 
 if __name__ == '__main__':
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5000, threaded=True)
 
