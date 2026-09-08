8323421f7e4de1f2e864aecf421254dfe3627ef7
iBug <git@ibugone.com>
2026-09-08 14:34:18 +0800

    Replace `run.py` with gunicorn

--- docker-startup.sh
+++ docker-startup.sh
@@ -4,4 +4,11 @@
 ln -sfn /usr/share/zoneinfo/"$TZ" /etc/localtime
 echo "$TZ" >/etc/timezone
 
-exec python3 run.py
+python3 -c 'from app import app, db; context = app.app_context(); context.push(); db.create_all(); context.pop()'
+
+exec gunicorn \
+    --bind 0.0.0.0:5000 \
+    --workers "${GUNICORN_WORKERS:-4}" \
+    --access-logfile - \
+    --error-logfile - \
+    run:app
